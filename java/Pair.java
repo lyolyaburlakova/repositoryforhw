8ee454271d5a866e6a1a2a5672d611c4373844cd
lyolyaburlakova <90637037+lyolyaburlakova@users.noreply.github.com>
2021-10-12 18:39:39 +0300

    Add files via upload

--- java/Pair.java
+++ java/Pair.java
@@ -1,3 +1,57 @@
-public class Pair {
+import java.util.Objects;
 
+public class Pair<X, Y> {
+    private X first;
+    private Y second;
+
+    private Pair(X first, Y second){
+        this.first = first;
+        this.second = second;
+    }
+
+    public X getFirst() {
+        return first;
+    }
+
+    public Y getSecond() {
+        return second;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pair pair = (Pair) o;
+        return Objects.equals(first, pair.first) && Objects.equals(second, pair.second);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(first, second);
+    }
+    public static Pair of(Object first, Object second){
+        return new Pair(first, second);
+    }
+
+    @Override
+    public String toString() {
+        return "pair{" +
+                "first=" + first +
+                ", second=" + second +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        Pair<Integer, String> pair = Pair.of(1, "hello");
+        Integer i = pair.getFirst(); // 1
+        String s = pair.getSecond(); // "hello"
+        System.out.println(i); // 1
+        System.out.println(s); // "hello"
+
+        Pair<Integer, String> pair2 = Pair.of(1, "hello");
+        boolean mustBeTrue = pair.equals(pair2); // true!
+        boolean mustAlsoBeTrue = pair.hashCode() == pair2.hashCode(); // true!
+        System.out.println(mustBeTrue); // true!
+        System.out.println(mustAlsoBeTrue); // true!
+    }
 }
